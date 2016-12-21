821956e4ede88e6d52734c481268e734b36565a9
Hallez <jbhallez@aol.com>
2016-12-21 11:31:05 +0100

    Permet de savoir quel jour c'est un anniversaire ou autre évenement marqué dans config.sh de Jarvis
    (20/12/2016: Anonnce le prochain evenement...)
    (18/12/2016: Correction d un bug entre 2 plugins...)
    
    Annonce combien de jour il reste avant un évenement enregistré dans le
    config.sh de jarvis.
    Pour les différents anniversaire ... date importante comme Noël...

--- config.sh
+++ config.sh
@@ -3,29 +3,6 @@
 ### Bien respecter pour un anniversaire ce protocole:  ...."nom": "lanniversaire de ...
 ### Si l'année est inférieur à l age actuelle Jarvis indiquera l AGE de la date en question...
 ### Afin de bien parametrer la diction, veuillez lancer Jarvis en mode  "Troubleshooting mode"  et prononcer votre phrase de vous recopierai dans le fichier config.sh
-##
-#cestpourquand='{ "devices":[
-#{ "voiciladate": "01/01/2017", "nom": "le jour de lAn"},
-#{ "voiciladate": "01/08/1975", "nom": "lanniversaire de Florent"},
-#{ "voiciladate": "01/13/1999", "nom": "lanniversaire de Morgane"},
-#{ "voiciladate": "04/02/2006", "nom": "lanniversaire de Alicia"},
-#{ "voiciladate": "04/29/2006", "nom": "lanniversaire de Justine"},
-#{ "voiciladate": "05/15/2009", "nom": "lanniversaire de Valentin"},
-#{ "voiciladate": "05/30/1974", "nom": "lanniversaire de Sousou"},
-#{ "voiciladate": "08/31/1974", "nom": "lanniversaire de Jibé"},
-#{ "voiciladate": "11/10/1981", "nom": "lanniversaire de Léa"},
-#{ "voiciladate": "11/13/2005", "nom": "lanniversaire de Noah"},
-#{ "voiciladate": "11/22/2007", "nom": "lanniversaire de Lorenzo"},
-#{ "voiciladate": "11/30/2016", "nom": "le rendez-vous chez le Dentiste"},
-#{ "voiciladate": "12/25/2016", "nom": "Noel"},
-#{ "voiciladate": "12/31", "nom": "Fin dannée"}
-#
-#]}'
-# Commencer par le mois, je jour et l année          ...voiciladate": "MOIS/JOUR/ANNEE"...
-# un aposthrophe se supprime ex:                     l'anniverdaire --> lanniversaire
-# Bien respecter pour un anniversaire ce protocole:  ...."nom": "lanniversaire de ...
-# Si l'année est inférieur à l age actuelle Jarvis indiquera l AGE de la date en question...
-# Afin de bien parametrer la diction, veuillez lancer Jarvis en mode  "Troubleshooting mode"  et prononcer votre phrase de vous recopierai dans le fichier config.sh
 
 cestpourquand='{ "devices":[
 { "voiciladate": "01/01/2017", "nom": "le jour de lAn"},
@@ -41,6 +18,6 @@ cestpourquand='{ "devices":[
 { "voiciladate": "11/22/2007", "nom": "lanniversaire de Lorenzo"},
 { "voiciladate": "11/30/2016", "nom": "le rendez-vous chez le Dentiste"},
 { "voiciladate": "12/25/2016", "nom": "Noel"},
-{ "voiciladate": "31/12", "nom": "Fin dannée"}
+{ "voiciladate": "12/31", "nom": "Fin dannée"}
 
 ]}'
